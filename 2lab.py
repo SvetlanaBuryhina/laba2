159b6d626f6fd3d8f98036999432e5057c0745f6
Burykhina <vatrushjkeee@mail.ru>
2016-11-09 17:36:26 +0300

    laba2
    laba2

---
+++ 2lab.py
@@ -0,0 +1,22 @@
+﻿import re
+f1= open('access.log' , 'r')
+unique=set()
+for line in f1.readlines():
+    pattern = '\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[‌​0-5]|2[0-4][0-9]|[01‌​]?[0-9][0-9]?)\\b'
+    ips = re.findall(pattern, line)
+    for i in range (len(ips)):
+        if ips[i] in unique:
+            pass
+        else:
+            unique.add(ips[i])
+subnet=set()
+for item in unique:
+    pattern = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}'
+    result = re.findall(pattern, item)
+    for i in range (len(result)):
+        subnet.add(result[i])
+for element in subnet:
+    for ip in unique:
+        if ip.startswith(element):
+            print(ip)
+    print(" ")  
